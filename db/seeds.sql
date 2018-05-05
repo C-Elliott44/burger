2a7cee1ca88991870894e0048d37859d207e8251
Cameron Elliott <CameronElliott@camerons-mbp-4.attlocal.net>
2018-05-04 23:09:42 -0700

    This Silly Thing

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheese Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Western BBQ Texas Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Savory Bacon Bree Burger", 0);
